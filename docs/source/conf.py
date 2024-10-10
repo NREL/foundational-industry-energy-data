6766912508f10ebdb3b4a1d211d35c6a9a2d9e38
McMillan, Colin <Colin.McMillan@nrel.gov>
2024-10-10 16:26:44 -0400

    Documentation updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'FIED'
+project = 'The Foundational Industry Energy Dataset (FIED)'
 copyright = '2024, Alliance for Sustainable Energy, LLC.'
 author = 'Alliance for Sustainable Energy, LLC.'
 release = '0.1'
@@ -14,15 +14,36 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.viewcode'
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'pandas': ('https://pandas.pydata.org/pandas-docs/version/1.2.0/', None),
+    'numpy': ('https://numpy.org/doc/1.23/reference/index.html', None)
+}
 
 templates_path = ['_templates']
-exclude_patterns = []
-
-
+exclude_patterns = [
+    "**.ipynb_checkpoints",
+    "**__pycache__**",
+    "**/includes/**"
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'press'
 html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+autoclass_content = "both"  # Add __init__ doc (ie. params) to class summaries
+autodoc_member_order = 'bysource'
+autodoc_inherit_docstrings = True  # If no docstring, inherit from base class
+add_module_names = False  # Remove namespaces from class/method signatures
