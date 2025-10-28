9271177f357e671bd445f6b2eb2c3fd9128c6a0c
Gui Castelao <guilherme@castelao.net>
2025-10-28 14:28:10 -0600

    refact: Moving datasets to allow sub-modules

---
+++ fied/datasets/__init__.py
@@ -0,0 +1,17 @@
+from .mod import (
+    fetch_frs,
+    fetch_zip_codes,
+    fetch_nei_2017,
+    fetch_nei_2020,
+    fetch_emission,
+    fetch_webfirefactors,
+    fetch_scc,
+    fetch_naics,
+    fetch_shapefile_census_block_groups,
+    fetch_shapefile_congressional_district,
+    fetch_shapefile_county,
+    fetch_shapefile_NHDP,
+    fetch_state_FIPS,
+    fetch_QPC,
+    fetch_ghgrp_records,
+)
